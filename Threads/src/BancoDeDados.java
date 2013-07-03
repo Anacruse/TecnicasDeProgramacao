553b8d7f548aac8d24dcbdf9868b7076db1f6f34
Gabriel Augusto <biel@live.in>
2013-07-03 15:59:39 -0300

    Questao 3

--- Threads/src/BancoDeDados.java
+++ Threads/src/BancoDeDados.java
@@ -1,10 +1,11 @@
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Vector;
 
 public class BancoDeDados {
 	public static void main(String[] args) throws InterruptedException {
 		
-		Collection<String> sqls = new HashSet<String>();
+		Collection<String> sqls = new Vector<String>();
 		
 		ProduzSQL p1 = new ProduzSQL(0, 10000, sqls);
 		Thread t1 = new Thread(p1);
